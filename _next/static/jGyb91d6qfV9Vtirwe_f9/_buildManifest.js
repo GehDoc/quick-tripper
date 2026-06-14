e08b64a3e891ba7c4b2e70ab3704c5d21c1c00c1
GehDoc <8092026+GehDoc@users.noreply.github.com>
2026-06-14 08:46:29 +0000

    Deploying to gh-pages from @ GehDoc/quick-tripper@2f47077f3c3ee52b3281fa919626c37fb54d1051 🚀

--- _next/static/jGyb91d6qfV9Vtirwe_f9/_buildManifest.js
+++ _next/static/jGyb91d6qfV9Vtirwe_f9/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/quick-tripper//_next/:path+",
+        "destination": "/quick-tripper/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
